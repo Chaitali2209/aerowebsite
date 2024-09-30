f861497775124a2e3aa66ba57315abadd8f93742
Chaitali2209 <chaitalipadalkar.it@bvcoenm.edu.in>
2024-09-30 14:19:23 +0530

    updated link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
-import './assets/css/tailwind.css'
+import './assets/css/tailwind.css';
 // import './assets/css/materialdesignicons.min.css'
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import IndexTwo from './pages/index-two';
 
 function App() {
   return (
-    <Routes>
-
-      <Route path='/' element={<IndexTwo/>}/>
-
-    </Routes>
+    <BrowserRouter basename="/aerowebsite">
+      <Routes>
+        <Route path='/' element={<IndexTwo />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
